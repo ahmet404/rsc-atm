c0d45dbd8d93db416606a4528c2021034ea631a3
ahmet404 <71425742+ahmet404@users.noreply.github.com>
2020-09-17 20:17:46 +0700

    Update atm_program.py

--- atm_program.py
+++ atm_program.py
@@ -16,7 +16,9 @@ while True:
             print("Anda sudah gagal 3x, ulangi lagi nanti")
             exit()
     while True:
-        print("Selamat datang di ATM RSC")
+        print("===============================")
+        print("== Selamat datang di ATM RSC ==")
+        print("===============================")
         menu_atm = ['Keluar','Cek Saldo','Debet','Simpan','Ganti Pin']
         i = 0
         for menu in menu_atm:
@@ -24,14 +26,14 @@ while True:
             i += 1
         pilih_menu = int(input("Pilih menu: "))
         if pilih_menu == 0:
-            print("Resi tercetak otomatis saat Anda keluar. \n Harap simpan tanda terima ini \n sebagai bukti transaksi Anda")
-            print("No. Record: ", random.randint(100000, 1000000))
-            print("Tanggal: ", datetime.datetime.now())
-            print("Saldo akhir: ", atm.checkBalance())
-            print("Terimakasih telah menggunakan atm rsc")
+            print("Resi tercetak otomatis saat Anda keluar.\nHarap simpan tanda terima ini\nsebagai bukti transaksi Anda")
+            print("No.Record:",random.randint(100000, 1000000))
+            print("Tanggal:", datetime.datetime.now())
+            print("Saldo akhir:", atm.checkBalance())
+            print("Terimakasih telah menggunakan ATM RSC")
             exit()
         elif pilih_menu == 1:
-            print("Saldo Anda : Rp.",atm.checkBalance())
+            print("Saldo Anda : Rp.",atm.checkBalance() , "\n")
         elif pilih_menu == 2:
             nominal = float(input("Masukkan nominal saldo: "))
             verify_debet = input("Konfirmasi: Anda akan melakukan debet dengan nominal berikut ? (y/n) "+ str(nominal) + " ")
@@ -50,6 +52,7 @@ while True:
             nominal = float(input("Masukkan nominal saldo: "))
             verify_deposit = input("Konfirmasi: Anda akan melakukan penyimpanan dengan nominal berikut ? (y/n) "+str(nominal) + " ")
             if verify_deposit == "y":
+                atm.saveBalance(nominal)
                 print("Saldo Anda sekarang adalah : Rp. ",atm.checkBalance())
             else:
                 break
@@ -57,14 +60,16 @@ while True:
         elif pilih_menu == 4:
             verify_pin = int(input("Masukkan pin Anda: "))
             while verify_pin != atm.checkPin():
-                print("Pin salah! Masukkan pin yang benar!")
-
+                print("Pin salah!")
+                verify_pin = int(input("Masukkan ulang pin Anda: "))
             updated_pin = int(input("Silahkan masukkan pin baru: "))
             print("Pin Anda berhasil diganti!")
             verify_newpin = int(input("Coba masukkan pin baru: "))
             if verify_newpin == updated_pin:
+                atm.pin = updated_pin
                 print("Pin baru Anda suskes!")
             else:
                 print("maaf, pin anda salah!")
         else:
             print("Maaf menu tidak tersedia!")
+# by Achmad Fauzi
